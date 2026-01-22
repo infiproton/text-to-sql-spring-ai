40edfa4bb9560f8087d624fc00243a9228079d41
Harish <harish.bn@gmail.com>
2026-01-22 16:19:45 +0530

    initial project setup

---
+++ src/test/java/com/nexacorp/ai/texttosql/TextToSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexacorp.ai.texttosql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextToSqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
